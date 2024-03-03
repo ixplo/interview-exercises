408e9482e36934a368732f3ae634360b504e3951
ixplo <ixplo@ya.ru>
2024-03-03 11:10:05 +0100

    5. code style

--- Serialization/src/main/java/serialization/SerializationManagement.java
+++ Serialization/src/main/java/serialization/SerializationManagement.java
@@ -6,16 +6,17 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.List;
 
 /**
  * @author Administrator
  */
 public class SerializationManagement {
-    static String fileFormat = "C:\\Serialized\\%1$s.dat";
+    private static final String FILE_FORMAT = "C:\\Serialized\\%1$s.dat";
 
-    public void Serialize(String dataIdentifier, Object dataObject) {
+    public void serialize(String dataIdentifier, Object dataObject) {
         System.out.printf("Started serialization to file: %s%n", dataIdentifier);
-        try (FileOutputStream file = new FileOutputStream(String.format(fileFormat, dataIdentifier));
+        try (FileOutputStream file = new FileOutputStream(String.format(FILE_FORMAT, dataIdentifier));
              ObjectOutputStream out = new ObjectOutputStream(file)) {
             out.writeObject(dataObject);
             System.out.println("Serialization finished successfully.");
@@ -28,10 +29,10 @@ public class SerializationManagement {
         }
     }
 
-    public Object Deserialize(String dataIdentifier) {
+    public <T> List<T> deserialize(String dataIdentifier, Class<T> objectClass) {
         System.out.printf("Started deserialization from file: %s%n", dataIdentifier);
         Object result = null;
-        try (FileInputStream file = new FileInputStream(String.format(fileFormat, dataIdentifier));
+        try (FileInputStream file = new FileInputStream(String.format(FILE_FORMAT, dataIdentifier));
              ObjectInputStream in = new ObjectInputStream(file)) {
             result = in.readObject();
             System.out.println("Deserialization finished successfully.");
@@ -44,6 +45,11 @@ public class SerializationManagement {
         } catch (Exception e) {
             System.out.printf("Deserialization failed with unexpected exception: %s", e);
         }
-        return result;
+        return castObject(objectClass, result);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> List<T> castObject(Class<T> clazz, Object object) {
+        return (List<T>) object;
     }
 }
--- SerializationExercises/src/main/java/exercises/TestApplication.java
+++ SerializationExercises/src/main/java/exercises/TestApplication.java
@@ -2,36 +2,31 @@ package exercises;
 
 import exercises.data.DataObject1;
 import exercises.data.DataObject2;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
 import serialization.SerializationManagement;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
- *
  * @author Administrator
  */
-public class TestApplication
-{
+public class TestApplication {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException, FileNotFoundException, ClassNotFoundException
-    {
+    public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         simpleSerializationTest();
         console.nextLine();
     }
 
-    private static void simpleSerializationTest() throws IOException, FileNotFoundException, ClassNotFoundException
-    {
+    private static void simpleSerializationTest() {
         SerializationManagement sm = new SerializationManagement();
-        ArrayList<DataObject1> dataObjects = new ArrayList<>();
-        
-        for (int x = 0; x < 100; x++)
-        {
+        List<DataObject1> dataObjects = new ArrayList<>();
+
+        for (int x = 0; x < 100; x++) {
             DataObject1 do1 = new DataObject1();
             dataObjects.add(do1);
             do1.setProperty1(String.valueOf(x));
@@ -41,21 +36,19 @@ public class TestApplication
             do1.getComplexTypeProperty().setProperty1(String.valueOf(x));
             do1.getComplexTypeProperty().setProperty2(x);
         }
-        
-        sm.Serialize("TestData", dataObjects);
-        dataObjects = null;
-        dataObjects = (ArrayList)sm.Deserialize("TestData");
-        
-        for (int x = 0; x < 100; x++)
-        {
+
+        sm.serialize("TestData", dataObjects);
+        dataObjects = sm.deserialize("TestData", DataObject1.class);
+
+        for (int x = 0; x < 100; x++) {
             DataObject1 do1 = dataObjects.get(x);
-            assert(do1.getProperty1().equals(String.valueOf(x)));
-            assert(do1.getProperty2() == x);
-            assert(do1.getComplexTypeProperty().getSourceIdentifier() == null);
-            assert(do1.getComplexTypeProperty().getProperty1().equals(String.valueOf(x)));
-            assert(do1.getComplexTypeProperty().getProperty2() == x);
+            assert (do1.getProperty1().equals(String.valueOf(x)));
+            assert (do1.getProperty2() == x);
+            assert (do1.getComplexTypeProperty().getSourceIdentifier() == null);
+            assert (do1.getComplexTypeProperty().getProperty1().equals(String.valueOf(x)));
+            assert (do1.getComplexTypeProperty().getProperty2() == x);
         }
         System.out.println("Assertions are passed");
     }
-    
+
 }
